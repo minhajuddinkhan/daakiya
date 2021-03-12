b0348cc8c457d7ba85559d35c38a0e47eddab105
Minhaj Uddin Khan <minhajuddinkhan75@gmail.com>
2021-03-12 20:48:44 +0500

    add readme

--- README.md
+++ README.md
@@ -18,15 +18,19 @@ Daakiya is a message broker that ensures 100% message delivery. It is inspired b
 
 ```go
 func main() {
-	reg := getRegistry()
+
+	//initializes daakiya backed by cassandra
+	dk := getDaakiya()
+
 	go func() {
+		//start an thread that pushes messages
+		//to daakiya every second
 		i := 0
 		for {
-
-			reg.Append(registry.AppendMessage{
+			dk.Append(registry.AppendMessage{
 				Topic: "TEST_TOPIC",
 				Hash:  "CLIENT_1",
-				Value: []byte(fmt.Sprintf("%d", i)),
+				Value: []byte(fmt.Sprintf("Hello my friend %d", i)), //Hello my friend {i}
 			})
 			time.Sleep(1 * time.Second)
 			i++
@@ -35,18 +39,22 @@ func main() {
 
 	ctx, cancelFunc := context.WithCancel(context.Background())
 	defer cancelFunc()
-	channel, err := reg.FromOffset(ctx, registry.Query{
+
+	//start reading messages from the provided offset
+	channel, err := dk.FromOffset(ctx, registry.Query{
 		Hash:   "CLIENT_1",
 		Topic:  "TEST_TOPIC",
 		Offset: registry.OLDEST,
 	})
+
 	if err != nil {
 		log.Fatal(err)
 	}
 
 	for {
 		msg := <-channel
-		fmt.Println(string(msg))
+		fmt.Println(string(msg)) //Hello my friend {i}
 	}
+
 }
 ```
--- examples/simple.go
+++ examples/simple.go
@@ -14,15 +14,18 @@ import (
 
 func main() {
 
-	reg := getRegistry()
+	//initializes daakiya backed by cassandra
+	dk := getDaakiya()
+
 	go func() {
+		//start an thread that pushes messages
+		//to daakiya every second
 		i := 0
 		for {
-
-			reg.Append(registry.AppendMessage{
+			dk.Append(registry.AppendMessage{
 				Topic: "TEST_TOPIC",
 				Hash:  "CLIENT_1",
-				Value: []byte(fmt.Sprintf("%d", i)),
+				Value: []byte(fmt.Sprintf("Hello my friend %d", i)), //Hello my friend {i}
 			})
 			time.Sleep(1 * time.Second)
 			i++
@@ -31,23 +34,26 @@ func main() {
 
 	ctx, cancelFunc := context.WithCancel(context.Background())
 	defer cancelFunc()
-	channel, err := reg.FromOffset(ctx, registry.Query{
+
+	//start reading messages from the provided offset
+	channel, err := dk.FromOffset(ctx, registry.Query{
 		Hash:   "CLIENT_1",
 		Topic:  "TEST_TOPIC",
 		Offset: registry.OLDEST,
 	})
+
 	if err != nil {
 		log.Fatal(err)
 	}
 
 	for {
 		msg := <-channel
-		fmt.Println(string(msg))
+		fmt.Println(string(msg)) //Hello my friend {i}
 	}
 
 }
 
-func getRegistry() daakiyaa.Daakiya {
+func getDaakiya() daakiyaa.Daakiya {
 
 	clusterConfig := gocql.NewCluster("localhost:9042")
 	clusterConfig.Keyspace = "test_keyspace"
